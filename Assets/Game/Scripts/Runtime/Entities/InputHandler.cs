690f72d181d163c7b37cde87f34fa114a60450ac
Paulo Oliveira <paulo@ninito.me>
2022-01-28 20:35:45 -0300

    Add base InputHandler class

---
+++ Assets/Game/Scripts/Runtime/Entities/InputHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Game.Editor.Entities
+{
+	/// <summary>
+	/// A component that handles input for an entity
+	/// </summary>
+	public abstract class InputHandler : MonoBehaviour
+	{
+		#region Properties
+
+		public bool InputSuspended { get; set; } = false;
+
+		#endregion
+
+		#region Unity Callbacks
+
+		public void Update()
+		{
+			CollectInput();
+
+			if (InputSuspended) return;
+			DistributeInput();
+		}
+
+		#endregion
+
+		#region Protected Methods
+
+		/// <summary>
+		/// Collects input
+		/// </summary>
+		protected abstract void CollectInput();
+		
+		/// <summary>
+		/// Distributes input to relevant components
+		/// </summary>
+		protected abstract void DistributeInput();
+
+		#endregion
+	}
+}
