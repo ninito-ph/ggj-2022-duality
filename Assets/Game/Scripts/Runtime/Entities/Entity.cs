195f19d0c3206c8bfed64ba8358a20d924ff48c5
Paulo Oliveira <paulo@ninito.me>
2022-01-28 20:15:28 -0300

    Add Entity class

---
+++ Assets/Game/Scripts/Runtime/Entities/Entity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Editor.Entities
+{
+	/// <summary>
+	/// An entity that can be killed
+	/// </summary>
+	public sealed class Entity : MonoBehaviour
+	{
+		#region Fields
+
+		[Header("Dependencies")]
+		[SerializeField]
+		private InputHandler inputHandler;
+
+		private event Action<Entity> _onDeath;
+
+		#endregion
+
+		#region Properties
+
+		public InputHandler InputHandler => inputHandler;
+
+		public event Action<Entity> OnDeath
+		{
+			add => _onDeath += value;
+			remove => _onDeath -= value;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public void Kill()
+		{
+			
+		}
+
+		#endregion
+	}
+}
