74acaaad8fff74ec831652ea6ab294c2c4e9a70e
Paulo Oliveira <paulo@ninito.me>
2022-01-29 17:02:08 -0300

    Add RayProfile class to describe a ray

---
+++ Assets/Game/Scripts/Runtime/Input/RayProfile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Game.Runtime.Input
+{
+	/// <summary>
+	/// A struct describing a ray
+	/// </summary>
+	public struct RayProfile
+	{
+		#region Fields
+
+		public readonly Vector2 StartPos;
+		public readonly Vector2 EndPos;
+		public readonly Vector2 Direction;
+
+		#endregion
+
+		#region Constructos
+
+		public RayProfile(Vector2 startPos, Vector2 endPos, Vector2 direction)
+		{
+			StartPos = startPos;
+			EndPos = endPos;
+			Direction = direction;
+		}
+
+		#endregion
+	}
+}
