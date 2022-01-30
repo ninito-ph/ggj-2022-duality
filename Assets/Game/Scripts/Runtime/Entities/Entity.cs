65fb30b43a25254d5a149694b649535781697601
Paulo Oliveira <paulo@ninito.me>
2022-01-30 02:13:24 -0300

    Update Entity class
    - Add bullet handling
    - Add type (Light/Dark) switching

--- Assets/Game/Scripts/Runtime/Entities/Entity.cs
+++ Assets/Game/Scripts/Runtime/Entities/Entity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Game.Runtime.Input;
+using Game.Runtime.Systems;
 using UnityEngine;
 
 namespace Game.Runtime.Entities
@@ -7,6 +8,7 @@ namespace Game.Runtime.Entities
 	/// <summary>
 	/// An entity that can be killed
 	/// </summary>
+	[RequireComponent(typeof(ManaWallet))]
 	public sealed class Entity : MonoBehaviour
 	{
 		#region Fields
@@ -15,12 +17,14 @@ namespace Game.Runtime.Entities
 		[SerializeField]
 		private InputHandler inputHandler;
 
+		private ManaWallet _wallet;
 		private event Action<Entity> _onDeath;
 
 		#endregion
 
 		#region Properties
 
+		public DualType Type { get; private set; } = DualType.Light;
 		public InputHandler InputHandler => inputHandler;
 
 		public event Action<Entity> OnDeath
@@ -31,8 +35,41 @@ namespace Game.Runtime.Entities
 
 		#endregion
 
+		#region Unity Callbacks
+
+		private void Awake()
+		{
+			TryGetComponent(out _wallet);
+		}
+
+		#endregion
+
 		#region Public Methods
 
+		/// <summary>
+		/// Flips the entity's type
+		/// </summary>
+		public void FlipType()
+		{
+			Type = Type == DualType.Light ? DualType.Dark : DualType.Light;
+		}
+		
+		/// <summary>
+		/// Handles a bullet collision
+		/// </summary>
+		/// <param name="bullet">The bullet to handle</param>
+		public void HandleBullet(Bullet.Bullet bullet)
+		{
+			if (bullet.Type != Type)
+			{
+				Kill();
+			}
+			else
+			{
+				_wallet.RestoreMana(bullet.ManaRestore);
+			}
+		}
+		
 		/// <summary>
 		/// Kills the entity
 		/// </summary>
@@ -40,6 +77,7 @@ namespace Game.Runtime.Entities
 		{
 			inputHandler.InputSuspended = true;
 			_onDeath?.Invoke(this);
+			Destroy(gameObject);
 		}
 
 		#endregion
